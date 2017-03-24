eb60f0e76c622b095f3c6bf976dc158b44572a4f
luguoquan <343965290>
2017-03-24 14:16:45 +0800

    新增一个鲁国权项目

--- luguoquan/src/main/java/huarun/luguoquan/MainActivity.java
+++ luguoquan/src/main/java/huarun/luguoquan/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //新增一个注释
+        //快捷键可以用不
         setContentView(R.layout.activity_main);
     }
 }
